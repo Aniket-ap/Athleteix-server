aad37cc162e569f70ab30457c240a335697117ef
Aniket Pradhan <aniketpradhan814@gmail.com>
2022-12-16 15:09:37 +0530

    added morgan package

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import morgan from "morgan"
 import authRoutes from "./routes/auth.js";
 
 dotenv.config();
@@ -14,6 +15,9 @@ mongoose
   .catch((err) => {
     console.log("DB ERROR => ", err);
   });
+
+// middlewares
+app.use(morgan("dev"))
 
 // router middleware
 app.use("/api", authRoutes);
