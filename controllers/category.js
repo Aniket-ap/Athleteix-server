410373a6e67679fdce06a2af0bbc255ee999aa2d
Aniket Pradhan <aniketpradhan814@gmail.com>
2022-12-17 14:21:25 +0530

    Category CRUD

--- controllers/category.js
+++ controllers/category.js
@@ -1,22 +1,69 @@
 import Category from "../models/category.js";
-import slugify from "slugify"
+import slugify from "slugify";
 
 export const create = async (req, res) => {
   try {
-    const {name} = req.body;
-    if(!name.trim()){
-        return res.json({error: "Name is required"})
+    const { name } = req.body;
+    if (!name.trim()) {
+      return res.json({ error: "Name is required" });
     }
-    const existingCategory = await Category.findOne({name});
-    if(existingCategory){
-        return res.json({error: "Already existe"})
+    const existingCategory = await Category.findOne({ name });
+    if (existingCategory) {
+      return res.json({ error: "Already existe" });
     }
 
-    const caregory = await new Category({name, slug: slugify(name)}).save();
-    res.json(caregory)
-
-  } catch (error) {
-    console.log(error);
+    const caregory = await new Category({ name, slug: slugify(name) }).save();
+    res.json(caregory);
+  } catch (err) {
+    console.log(err);
     return res.status(401).json(err);
+  }
+};
+
+export const update = async (req, res) => {
+  try {
+    const { name } = req.body;
+    const category = await Category.findByIdAndUpdate(
+      req.params.categoryId,
+      {
+        name,
+        slug: slugify(name),
+      },
+      { new: true }
+    );
+    res.json(category);
+  } catch (err) {
+    console.log(err);
+    return res.status(401).json(err.message);
+  }
+};
+
+export const remove = async (req, res) => {
+  try {
+    const removed = await Category.findByIdAndDelete(req.params.categoryId);
+    res.json(removed);
+  } catch (err) {
+    console.log(err);
+    return res.status(401).json(err.message);
+  }
+};
+
+export const list = async (req, res) => {
+  try {
+    const all = await Category.find({});
+    res.json(all);
+  } catch (err) {
+    console.log(err);
+    return res.status(401).json(err.message);
+  }
+};
+
+export const read = async (req, res) => {
+  try {
+    const category = await Category.findOne({ slug: req.params.slug });
+    return res.json(category);
+  } catch (err) {
+    console.log(err);
+    return res.status(401).json(err.message);
   }
 };
--- routes/category.js
+++ routes/category.js
@@ -6,8 +6,12 @@ const router = express.Router();
 import { requereSignIn, isAdmin } from "../middlewares/auth.js";
 
 // controllers
-import {create} from "../controllers/category.js"
+import {create, update, remove, list, read} from "../controllers/category.js"
 
 router.post("/category", requereSignIn, isAdmin, create)
+router.put("/category/:categoryId", requereSignIn, isAdmin, update)
+router.delete("/category/:categoryId", requereSignIn, isAdmin, remove)
+router.get("/categories", list)
+router.get("/category/:slug", read)
 
 export default router
