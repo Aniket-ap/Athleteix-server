8200a12e6ad4e1e7034430e64e91c011ccee5f96
Aniket Pradhan <aniketpradhan814@gmail.com>
2022-12-18 11:26:37 +0530

    cors package added and applied cors

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import morgan from "morgan"
+import cors from "cors"
 import authRoutes from "./routes/auth.js";
 import categoryRoutes from "./routes/category.js";
 import productRoutes from "./routes/product.js";
@@ -19,6 +20,7 @@ mongoose
   });
 
 // middlewares
+app.use(cors());
 app.use(morgan("dev"))
 app.use(express.json())
 
